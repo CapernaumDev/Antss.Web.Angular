27f635019d0cbe60fe30b19200e0b13916d4bd17
David <davidsutcliffe@mail.com>
2022-02-12 19:56:18 +0000

    filter source tests

---
+++ src/app/core/directives/filter-source.spec.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FilterSourceDirective } from './filter-source.directive';
+
+@Component({
+  template: `<input filterSource/>`
+})
+class TestComponent { }
+
+describe('FilterSourceDirective', () => {
+  let directiveInstance: FilterSourceDirective;
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ 
+        TestComponent, FilterSourceDirective
+      ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    const directiveEl = fixture.debugElement.query(By.directive(FilterSourceDirective));
+    directiveInstance = directiveEl.injector.get(FilterSourceDirective);
+    fixture.detectChanges();
+  });
+
+  it('should emit filter event with filter term when filter is called', () => {
+    spyOn(directiveInstance.filterChange, 'emit');
+    directiveInstance.filter('abc');
+    fixture.detectChanges();
+
+    expect(directiveInstance.filterChange.emit).toHaveBeenCalledOnceWith({ filterTerm: 'abc' });
+  });
+
+});
