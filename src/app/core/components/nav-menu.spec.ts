2512971c555ab92f6e8e8ec606f91066e9f42889
David <davidsutcliffe@mail.com>
2022-02-12 16:53:38 +0000

    nav menu tests

--- src/app/core/components/nav-menu.spec.ts
+++ src/app/core/components/nav-menu.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { By } from '@angular/platform-browser';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { CurrentUser } from '../models/user/current-user';
+import { selectCurrentUser } from '../store/selectors';
 
 import { NavMenuComponent } from './nav-menu.component';
 
@@ -9,19 +12,173 @@ describe('NavMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [provideMockStore({})],
+      providers: [provideMockStore({  })],
       declarations: [ NavMenuComponent ]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
+  it('should create', () => {
     fixture = TestBed.createComponent(NavMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe("when not logged in", () => {
+    beforeEach(() => {
+      const mockStore = TestBed.inject(MockStore);
+      const mockCurrentUserState = mockStore.overrideSelector(
+        selectCurrentUser,
+        null
+      );
+
+      fixture = TestBed.createComponent(NavMenuComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should show Login link', () => {
+      const loginLinkElement = fixture.debugElement.query(
+        By.css('[href="/login"]')
+      );
+  
+      expect(loginLinkElement).toBeTruthy();
+    });
+  
+    it('should show Home link', () => {
+      const homeLinkElement = fixture.debugElement.query(
+        By.css('[href="/"]')
+      );
+  
+      expect(homeLinkElement).toBeTruthy();
+    });
+  
+    it('should show Create Ticket link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/create-ticket"]')
+      );
+  
+      expect(createTicketLinkElement).toBeTruthy();
+    });
+
+    it('should not show Admin link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/user-list"]')
+      );
+  
+      expect(createTicketLinkElement).toBeNull();
+    });
+
+    it('should not show Tickets link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/ticket-list"]')
+      );
+  
+      expect(createTicketLinkElement).toBeNull();
+    });
+
+    it('should not show Board link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/ticket-board"]')
+      );
+  
+      expect(createTicketLinkElement).toBeNull();
+    });
+
+    it('should not show Logout link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[data-test-id="logoutLink"]')
+      );
+  
+      expect(createTicketLinkElement).toBeNull();
+    });
+
+    it('should not show My Profile link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/my-profile"]')
+      );
+  
+      expect(createTicketLinkElement).toBeNull();
+    });
+  });
+
+  describe("when logged in not as admin", () => {
+    beforeEach(() => {
+      const mockStore = TestBed.inject(MockStore);
+      const user = new CurrentUser();
+      user.id = 1;
+      const mockCurrentUserState = mockStore.overrideSelector(
+        selectCurrentUser, user
+      );
+
+      fixture = TestBed.createComponent(NavMenuComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should not show Admin link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/user-list"]')
+      );
+  
+      expect(createTicketLinkElement).toBeNull();
+    });
+
+    it('should show Tickets link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/ticket-list"]')
+      );
+  
+      expect(createTicketLinkElement).toBeTruthy();
+    });
+
+    it('should show Board link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/ticket-board"]')
+      );
+  
+      expect(createTicketLinkElement).toBeTruthy();
+    });
+
+    it('should show Logout link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[data-test-id="logoutLink"]')
+      );
+  
+      expect(createTicketLinkElement).toBeTruthy();
+    });
+
+    it('should show My Profile link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/my-profile"]')
+      );
+  
+      expect(createTicketLinkElement).toBeTruthy();
+    });
+  });
+
+  describe("when logged in as admin", () => {
+    beforeEach(() => {
+      const mockStore = TestBed.inject(MockStore);
+      const user = new CurrentUser();
+      user.id = 1;
+      user.userType = 'Admin';
+      const mockCurrentUserState = mockStore.overrideSelector(
+        selectCurrentUser, user
+      );
+
+      fixture = TestBed.createComponent(NavMenuComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should show Admin link', () => {
+      const createTicketLinkElement = fixture.debugElement.query(
+        By.css('[href="/user-list"]')
+      );
+  
+      expect(createTicketLinkElement).toBeTruthy();
+    });
   });
 });
